c55776deac27050e20540d70cdc2d819acbd548a
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2023-02-13 21:49:44 -0300

    Se agrega la gestion a traves de comandos para el temporizador en el chat

--- main.js
+++ main.js
@@ -1,52 +1,101 @@
 const timerDisplay = document.querySelector("#timer");
 const pomo = document.querySelector("#pomo-num");
 const etiqueta = document.querySelector('#etiqueta')
-let timer = 1 * 60;
-let pomodoros = 1;
+import tmi from 'tmi.js'
+var sound = new Audio("music.mp3");
+
+
+// Variables necesarias
+var timer = 60 * 60;
+var pomoCount = 0;
 const pomodoroTotal = 12;
-let breakTime = false;
-
-
+var interval;
+var minutos;
+var segundos;
+var savedTimer = 0;
+// Función para iniciar la cuenta regresiva
 function startTimer() {
-  var sound = new Audio("music.mp3");
-  const minutes = Math.floor(timer / 60);
-  const seconds = timer % 60;
-  timerDisplay.innerHTML = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""
-    }${seconds}`;
-  timer--;
-  if (timer < 0) {
-    if (!breakTime) {
-      pomodoros++;
-      pomo.innerHTML = pomodoros;
-      etiqueta.innerHTML = 'Break'
-      sound.play();
-      if (pomodoros === pomodoroTotal) {
+  interval = setInterval(function () {
+    minutos = parseInt(timer / 60, 10);
+    segundos = parseInt(timer % 60, 10);
+
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    timerDisplay.innerHTML = minutos + ":" + segundos;
+
+    if (--timer < 0) {
+      timer = 60 * 60;
+      sound.play()
+
+      if (pomoCount === pomodoroTotal) {
+        clearInterval(interval);
+        console.log("Se completaron los 12 pomos");
+        sound.play()
         return;
       }
-      setTimeout(() => {
-        timer = 10 * 60;
-        breakTime = true;
-        startTimer();
-        sound.play();
-      }, 60 * 1000);
-    } else {
-      breakTime = false;
-      timer = 60 * 60;
-      etiqueta.innerHTML = 'Productivo'
-      setTimeout(() => {
-        sound.play();
-        startTimer();
-      }, 10 * 60 * 1000);
-    }
-  } else {
-    setTimeout(startTimer, 1000);
-  }
 
+      // Iniciar cuenta regresiva de 10 minutos
+      timer = 10 * 60;
+      pomoCount++;
+      pomo.innerHTML = pomoCount
+
+    }
+  }, 1000);
 }
 
-startTimer();
+// Función para detener el timer
+function stopTimer() {
+  savedTimer = timer;
+  clearInterval(interval);
+  sound.play()
+}
+
+// Función para reiniciar el timer
+function restartTimer() {
+  stopTimer();
+  timer = savedTimer;
+  pomoCount = 0;
+  startTimer();
+  sound.play()
+}
+
+function restartBreak() {
+  stopTimer();
+  timer = 10 * 60;
+  pomoCount = 0;
+  startTimer();
+  sound.play()
+}
 
 
+
+// Conectar a Twitch a través de tmi.js
+const client = new tmi.Client({
+  channels: ['brunispet']
+});
+
+
+client.connect();
+
+// Escuchar comandos en Twitch
+client.on("chat", function (channel, userstate, message, self) {
+  const username = userstate.username;
+  const mod = userstate?.mod
+  if (username === 'cuartodechenz' || mod) {
+    if (message === "!start") {
+      startTimer();
+    } else if (message === "!pause") {
+      stopTimer();
+    } else if (message === "!restart") {
+      restartTimer();
+    } else if (message === "!restartbreak") {
+      restartBreak();
+    }
+  }
+});
+
+startTimer()
 
 
 
